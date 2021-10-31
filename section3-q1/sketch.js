2cabd71df88fd84544f80ee4b8ed40be25d2325e
suzukat <suzukagozen224@gmail.com>
2021-11-01 01:48:12 +0900

    CHANGE2 section3-q1
    練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」を提出してしまったので、スペースキーで速くなるように変えておきました。

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -10,11 +10,16 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  if(keyIsDown(" ".charCodeAt(0)))
+  {count = (count +3) % cycle;}
+  else
+  {count= (count +1) % cycle;}
 
-   // BLANK[1]
-   if(count>40 &&count<=50){ellipse(width / 2, height / 2,count);}
-   else if(count>50 && count<=60){ellipse(width / 2, height / 2, 100-count);}
-   else {ellipse(width / 2, height / 2, 40);}
+
+  if(count>40 &&count<=50)
+   {ellipse(width / 2, height / 2,count);}
+   else if(count>50 && count<=60)
+   {ellipse(width / 2, height / 2, 100-count);}
+   else{ellipse(width / 2, height / 2, 40);}
 
 }
